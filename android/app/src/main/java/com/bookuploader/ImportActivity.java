44560aea24be446b1651e260946ab3b835dc3ff5
Denis Moiseev <isdenmois@gmail.com>
2020-04-29 22:52:07 +0000

    Import dialog markup

--- android/app/src/main/java/com/bookuploader/ImportActivity.java
+++ android/app/src/main/java/com/bookuploader/ImportActivity.java
@@ -10,9 +10,12 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.InputStream;
 import java.io.OutputStream;
@@ -23,51 +26,69 @@ public class ImportActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_import);
+
+        findViewById(R.id.noButton).setOnClickListener(view -> finish());
+        findViewById(R.id.yesButton).setOnClickListener(view -> startImport());
     }
 
     @Override
     protected void onResume() {
         super.onResume();
+
         Uri data = getIntent().getData();
 
-        if(data != null) {
+        if (data == null) {
+            Toast.makeText(this, "Нет входных параметров для импорта", Toast.LENGTH_SHORT).show();
+            finish();
+        } else {
+            ContentResolver cr = getApplicationContext().getContentResolver();
+            String fileName = getContentName(cr, data);
+
+            ((TextView)findViewById(R.id.fileName)).setText(fileName);
+        }
+    }
+
+    private void startImport() {
+        Uri data = getIntent().getData();
+        final String scheme = data.getScheme();
+
+        findViewById(R.id.confirm).setVisibility(View.GONE);
+        findViewById(R.id.progressBar).setVisibility(View.VISIBLE);
+
+        setTitle("Importing...");
+
+        if (ContentResolver.SCHEME_CONTENT.equals(scheme)) {
             try {
                 importData(data);
-            }catch (Exception e) {
-                Log.e("File Import Error", e.getMessage());
-            }
 
-            Toast.makeText(this, "Файл импортирован", Toast.LENGTH_SHORT).show();
+                Toast.makeText(this, "Файл успешно импортирован", Toast.LENGTH_SHORT).show();
+            } catch (Exception e) {
+                Log.e("File Import Error", e.getMessage());
+                Toast.makeText(this, "Не удалось импортировать файл", Toast.LENGTH_SHORT).show();
+            }
+        } else {
+            Toast.makeText(this, "Неправильная схема для импорта: " + scheme, Toast.LENGTH_SHORT).show();
         }
 
         finish();
     }
 
-    private void importData(Uri data) {
-        final String scheme = data.getScheme();
+    private void importData(Uri data) throws FileNotFoundException, PackageManager.NameNotFoundException {
+        ContentResolver cr = getApplicationContext().getContentResolver();
+        InputStream is = cr.openInputStream(data);
+        if (is == null) return;
 
-        if (ContentResolver.SCHEME_CONTENT.equals(scheme)) {
-            try {
-                ContentResolver cr = getApplicationContext().getContentResolver();
-                InputStream is = cr.openInputStream(data);
-                if(is == null) return;
+        String name = getContentName(cr, data);
 
-                String name = getContentName(cr, data);
+        PackageManager m = getPackageManager();
+        String s = getPackageName();
+        PackageInfo p = m.getPackageInfo(s, 0);
+        s = p.applicationInfo.dataDir;
 
-                Log.d("BookUploader@Main", "mimeType: " + cr.getType(data) + "; host: " + data.getHost() + "; path: " + data.getPath() + "; name: "  + name);
-                PackageManager m = getPackageManager();
-                String s = getPackageName();
-                PackageInfo p = m.getPackageInfo(s, 0);
-                s = p.applicationInfo.dataDir;
-
-                InputStreamToFile(is, s + "/files/" + name);
-            } catch (Exception e) {
-                Log.e("File Import Error", e.getMessage());
-            }
-        }
+        InputStreamToFile(is, s + "/files/" + name);
     }
 
-    private String getContentName(ContentResolver resolver, Uri uri){
+    private String getContentName(ContentResolver resolver, Uri uri) {
         Cursor cursor = resolver.query(uri, null, null, null, null);
         cursor.moveToFirst();
         int nameIndex = cursor.getColumnIndex(MediaStore.MediaColumns.DISPLAY_NAME);
@@ -90,8 +111,7 @@ public class ImportActivity extends AppCompatActivity {
             }
 
             out.close();
-        }
-        catch (Exception e) {
+        } catch (Exception e) {
             Log.e("MainActivity", "InputStreamToFile exception: " + e.getMessage());
         }
     }
