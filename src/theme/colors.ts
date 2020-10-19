63a167f226573245ffdd2912a267f417801c6aa5
Denis Moiseev <isdenmois@gmail.com>
2020-10-19 20:44:45 +0000

    update theme

--- src/theme/colors.ts
+++ src/theme/colors.ts
@@ -7,3 +7,25 @@ export const red = '#f44336';
 export const darkBlue = '#007ac1';
 export const blue = '#2196f3';
 export const lightBlue = '#67daff';
+
+export const Background = '#F9FAFE';
+export const SearchBackground = '#B8D5F9';
+export const UploadBackground = '#C2B9F9';
+export const ProfileBackground = '#FFAD99';
+export const TabsBackground = 'white';
+
+export const SearchText = '#092D5D';
+export const UploadText = '#15085E';
+export const ProfileText = '#661400';
+export const TabText = '#121417';
+
+export const SearchSelected = '#5396EE';
+export const UploadSelected = '#6851F0';
+export const ProfileSelected = '#FF6842';
+
+export const Secondary = '#787D87';
+export const Search = '#8C93A3';
+export const Input = 'white';
+export const DeleteButton = '#9BA8CA';
+export const Success = '#48B755';
+export const Error = '#FD4463';
