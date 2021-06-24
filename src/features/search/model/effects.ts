839a6590cf577524d8cc9eb5553458e7217d8221
Denis Moiseev <isdenmois@gmail.com>
2021-06-24 14:33:10 +0300

    Removed shivering on upload tab

--- src/features/search/model/effects.ts
+++ src/features/search/model/effects.ts
@@ -1,6 +1,6 @@
 import { createEffect } from 'effector'
 
-import { bookSearch, ProviderType } from 'shared/api'
+import { api, ProviderType } from 'shared/api'
 import { $searchFilters } from 'entities/search-filters'
 
 import { $booksFound } from './stores'
@@ -12,7 +12,7 @@ type FetchBookParams = {
 }
 
 export const fetchBookItemsFx = createEffect(({ type, query, extension }: FetchBookParams) =>
-  bookSearch(type, query, extension),
+  api.bookSearch(type, query, extension),
 )
 
 $booksFound.on(fetchBookItemsFx.doneData, (_, books) => books)
--- src/features/upload-files/ui/upload-list.tsx
+++ src/features/upload-files/ui/upload-list.tsx
@@ -2,23 +2,15 @@ import React, { FC } from 'react'
 import { useFocusEffect } from '@react-navigation/native'
 import { useList, useStore } from 'effector-react'
 
-import { ActivityIndicator } from 'shared/ui'
 import { FileItem, shareFile } from 'entities/file'
-import { $files, $uploadState, fetchFiles, fetchFilesFx, parseFile, removeFile } from '../model'
+import { $files, $uploadState, fetchFiles, parseFile, removeFile } from '../model'
 
 export const UploadList: FC = () => {
-  const isLoading = useStore(fetchFilesFx.pending)
   const state = useStore($uploadState)
 
   useFocusEffect(fetchFiles)
 
-  const fileList = useList($files, file => (
+  return useList($files, file => (
     <FileItem file={file} disabled={state !== 'IDLE'} onParse={parseFile} onRemove={removeFile} onShare={shareFile} />
   ))
-
-  if (isLoading) {
-    return <ActivityIndicator color='uploadSelected' />
-  }
-
-  return fileList
 }
