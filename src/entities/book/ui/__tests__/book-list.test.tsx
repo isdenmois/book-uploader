a928d76a6f21130d9548f47ba753ddd9b579593e
Denis Moiseev <isdenmois@gmail.com>
2021-06-27 09:39:13 +0300

    Added entities/book tests

---
+++ src/entities/book/ui/__tests__/book-list.test.tsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import { act, fireEvent, render } from '@testing-library/react-native'
+
+import { BookItem } from 'shared/api'
+import { ThemeProvider } from 'shared/ui'
+
+import { BookList } from '../book-list'
+
+test('BookItem', async () => {
+  const books: BookItem[] = [
+    { ext: 'epub', type: 'zlib', title: 'First result', link: 'http://example.com' },
+    { ext: 'epub', type: 'zlib', title: 'Second', link: 'http://ex.com' },
+    { ext: 'epub', type: 'zlib', title: 'Third res', link: 'http://q.com' },
+  ]
+  const onDownload = jest.fn()
+
+  const { rerender, toJSON, findAllByTestId, unmount } = render(
+    <BookList books={null} isLoading onDownload={onDownload} />,
+    {
+      wrapper: ThemeProvider,
+    },
+  )
+
+  expect(toJSON()).toMatchSnapshot()
+
+  rerender(<BookList books={null} isLoading={false} onDownload={onDownload} />)
+  expect(toJSON()).toMatchSnapshot()
+
+  rerender(<BookList books={books} isLoading={false} onDownload={onDownload} />)
+  expect(toJSON()).toMatchSnapshot()
+
+  fireEvent.press((await findAllByTestId('book-item'))[0])
+
+  expect(onDownload).toHaveBeenCalledWith(books[0])
+
+  await act(async () => {
+    unmount()
+  })
+})
